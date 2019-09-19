d8c9571f4f4ec6648adfed7b69b45118222dbe78
Jared Baumann <jaredbaumann@MBP.local>
2019-09-19 11:12:27 -0500

    Added registration form, and removed commented out code.

--- app/routes.py
+++ app/routes.py
@@ -9,10 +9,8 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    user = {'username':'testusr'}
-    return render_template('index.html',title='HOME:P2',user=user)
-#def index():
-#    return "Hello, World!"
+    return render_template('index.html',title='HOME:P2')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -21,20 +19,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            #flash('Invalid username or password')
-            #return redirect('/login')
             return render_template('login.html', form=form, error='Invalid username or password')
         login_user(user)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = '/index'
         return redirect(next_page)
-        #return redirect('/index')
     return render_template('login.html', form=form)
-    #if form.validate_on_submit():
-    #    flash('Login requested for user {}'.format(form.username.data))
-    #    return redirect('/index')
-    #return render_template('login.html',form=form)
 
 @app.route('/logout')
 def logout():
