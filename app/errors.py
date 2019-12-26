3f6cdf58e980f6da40de2f9203db8834712f840c
Jared <jaredtb00@gmail.com>
2019-12-26 12:36:49 -0600

    Improved error handling by making the internal server error template more generic

--- app/errors.py
+++ app/errors.py
@@ -3,4 +3,8 @@ from app import flask_app as app, db
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('error.html', err_msg = "An unexpected error has occurred"), 500
+@app.errorhandler(404)
+def not_found_error(error):
+    db.session.rollback()
+    return render_template('error.html', err_msg = "The resource you requested was not available"), 404
